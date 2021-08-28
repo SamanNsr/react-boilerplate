ebe1bc5ff1cebcab7206cb0d7c8b21527ffad5b0
Saman Nsr <1999saman@gmail.com>
2021-08-29 03:35:15 +0430

    add an example reducer

---
+++ src/reducers/exampleReducer.js
@@ -0,0 +1,15 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state;
+    case 'REMOVE':
+      return state;
+    case 'TOGGLE':
+      return state;
+    case 'EDIT':
+      return state;
+    default:
+      return state;
+  }
+};
+export default reducer;
